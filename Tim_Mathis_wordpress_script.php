d437a38c7acd4f445b38b3c7a31cbdf6c8b908f5
Tim Mathis <timothymathis@gmail.com>
2018-07-27 06:16:22 -0500

    Removed irrelevant PHP file

--- Tim_Mathis_wordpress_script.php
+++
@@ -1,6 +0,0 @@
-if (!is_admin()) add_action("wp_enqueue_scripts", "load_latest_jquery", 11);
-function load_latest_jquery() {
-   wp_deregister_script('jquery');
-   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", false, null);
-   wp_enqueue_script('jquery');
-}
